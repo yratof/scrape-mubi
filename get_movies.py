636b102b4d019b4edb0b73d96a8881e6d3801f11
Andrew Smith <andrew.smith@frameworks.no>
2024-04-02 17:19:34 +0200

    Fixed the issues that stopped it from working in 2024

--- get_movies.py
+++ get_movies.py
@@ -1,37 +1,44 @@
 import requests
+from bs4 import BeautifulSoup
 import subprocess
 import json
 import ast
 
-r = requests.get('https://mubi.com/showing').text
 
-r = r.split('</script><script nomodule="" src="/_next/', 2)[0].split('script id="__NEXT_DATA__" type="application/json">', 2)[1]
-d = json.loads(r)
-m = d['props']['initialState']['filmProgramming']['filmProgrammingsByChannel']['0']
+url = 'https://mubi.com/en/no/collections/new-on-mubi'
+response = requests.get(url)
+html_content = response.text
+soup = BeautifulSoup(html_content, 'html.parser')
+script_tag = soup.find('script', type='application/json')
+json_text = script_tag.string  # or script_tag.contents[0] if .string doesn't work
+r = json.loads(json_text)
+
+# print(r);
+
+m = r['props']['initialProps']['pageProps']['collection']['films']
 
 movies = []
 movie_list = {}
 
 for movie in range(0, len(m)):
-    movies.append(m[movie]['film'])
+    movies.append(m[movie])
 
 
 for movie in range(0,len(movies)):
     movie_list[movie] = {}
-    movie_list[movie]['title'] = movies[movie]['title']
-    movie_list[movie]['director'] = movies[movie]['directors']
-    movie_list[movie]['plot'] = movies[movie]['short_synopsis']
+    movie_list[movie]['title'] = movies[movie]['original_title']
+    movie_list[movie]['directors'] = movies[movie]['directors']
+    movie_list[movie]['short_synopsis'] = movies[movie]['short_synopsis']
     movie_list[movie]['year'] = str(movies[movie]['year'])
-    movie_list[movie]['country'] = movies[movie]['country']
+    movie_list[movie]['country'] = movies[movie]['title_locale']
 
-    print('####################################################')
+    print('################### MOVIE META ######################')
     print(' ')
     print('title: ' + movie_list[movie]['title'])
-    print('director: ' + movie_list[movie]['director'])
-    print('plot: ' + movie_list[movie]['plot'])
+    print('short_synopsis: ' + movie_list[movie]['short_synopsis'])
     print('year: ' + movie_list[movie]['year'])
     print('country: ' + movie_list[movie]['country'])
-    print('----------------------------------------------------')
+    print('------------------ MAGNET META ---------------------')
 
     try:
         title = movie_list[movie]['title']
@@ -41,17 +48,18 @@ for movie in range(0,len(movies)):
         print(year)
         title = title.replace("'", "")
 
-        torrent = subprocess.check_output(['python2.7', 'torrent-hound.py','-q', title + ' ' + year]).decode()
+        torrent = subprocess.check_output(['python', 'torrent-hound.py','-q', title + ' ' + year]).decode()
         torrent = ast.literal_eval(torrent)
 
-        movie_list[movie]['magnetlink'] = torrent['sky']['results']['0']['magnet']
-        movie_list[movie]['magnetlinktitle'] = torrent['sky']['results']['0']['name'] + ' ratio: ' + torrent['sky']['results']['0']['ratio'] + ' seeders: '+ torrent['sky']['results']['0']['seeders']
+        movie_list[movie]['magnetlink'] = torrent['1337x']['results']['0']['magnet']
+        movie_list[movie]['magnetlinktitle'] = str(torrent['1337x']['results']['0']['name']) + ' ratio: ' + str(torrent['1337x']['results']['0']['ratio']) + ' seeders: '+ str(torrent['1337x']['results']['0']['seeders'])
+
         print('magnetlinktitle: '+ movie_list[movie]['magnetlinktitle'])
         print('magnetlink: \n'+ movie_list[movie]['magnetlink'])
     except Exception as e:
         print(e)
-        movie_list[movie]['magnetlink'] = 'N/A'
-        movie_list[movie]['magnetlinktitle'] = 'N/A'
+        movie_list[movie]['magnetlink'] = 'Nothing found '
+        movie_list[movie]['magnetlinktitle'] = '¯\_(ツ)_/¯'
         print('magnetlinktitle: '+ movie_list[movie]['magnetlinktitle'])
         print('magnetlink: '+ movie_list[movie]['magnetlink'])
         print('####################################################')
